db46c6ba6f2ab852bde8df48fd8bb2b9a20980be
Elena Stepanova <tay787@yandex.ru>
2023-04-19 21:57:24 +0300

    Добавляет ведьмаков

--- source/js/modules/slider/initSwipers.js
+++ source/js/modules/slider/initSwipers.js
@@ -1,6 +1,20 @@
 const initSwipers = () => {
+  if (document.querySelector('.coaches__swiper')) {
+    const coachesSwiper = new Swiper ('.coaches__swiper', { // eslint-disable-line
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 'auto',
+
+
+      navigation: {
+        nextEl: '.coaches__button--next',
+        prevEl: '.coaches__button--prev',
+      },
+    });
+  }
+
   if (document.querySelector('.comments__swiper')) {
-    const commentsSwiper = new Swiper('.comments__swiper', {
+    const commentsSwiper = new Swiper('.comments__swiper', { // eslint-disable-line
       direction: 'horizontal',
       loop: false,
       slidesPerView: 'auto',
