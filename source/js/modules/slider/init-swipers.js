1e2134078f4930f50fb6f92df5f73dc9096ef88f
Elena Stepanova <tay787@yandex.ru>
2023-04-27 10:19:44 +0300

    Правки после теста

--- source/js/modules/slider/init-swipers.js
+++ source/js/modules/slider/init-swipers.js
@@ -3,7 +3,25 @@ const initSwipers = () => {
     const coachesSwiper = new Swiper ('.coaches__swiper', { // eslint-disable-line
       direction: 'horizontal',
       loop: true,
-      slidesPerView: 'auto',
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+        },
+        768: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          spaceBetween: 30,
+        },
+        1200: {
+          slidesPerView: 3,
+          slidesPerGroup: 1,
+        },
+        1280: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+        },
+      },
 
       navigation: {
         nextEl: '.coaches__button--next',
